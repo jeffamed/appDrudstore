7fe89ce07ff923a7ab0a5dd933ed69b701265fa0
Yefry Moraga <jeffamed@gmail.com>
2021-10-20 12:11:08 -0600

    usando guarded

--- app/Models/Laboratory.php
+++ app/Models/Laboratory.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Laboratory extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
--- app/Models/Usage.php
+++ app/Models/Usage.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Usage extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
 }
