077bb3193f94590b169980df3835a870539f3901
Yefry Moraga <jeffamed@gmail.com>
2021-12-24 07:45:47 -0600

    dado diseño a la notas informativa del dashboar

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -4,11 +4,12 @@
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row no-gutters border border-success">
                     <div class="col-md-4 bg-success">
-                        <img src="" class="card-img" alt="">
+                        <i class="fa fa-shopping-basket fa-3x my-3 d-block text-center" aria-hidden="true"></i>
                     </div>
                     <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                        <div class="card-body p-1 text-center">
+                            <h3 class="card-title mb-1">35</h3>
+                            <p class="text-muted mb-1 "><small class="font-weight-bold">Ventas del Dia</small></p>
                         </div>
                     </div>
                 </div>
@@ -18,11 +19,12 @@
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row no-gutters border border-warning">
                     <div class="col-md-4 bg-warning">
-                        <img src="" class="card-img" alt="">
+                        <i class="fa fa-line-chart fa-3x my-3 d-block text-center" aria-hidden="true"></i>
                     </div>
                     <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                        <div class="card-body p-1 text-center">
+                            <h3 class="card-title mb-1">135</h3>
+                            <p class="text-muted mb-1 "><small class="font-weight-bold">Ventas de la Semana</small></p>
                         </div>
                     </div>
                 </div>
