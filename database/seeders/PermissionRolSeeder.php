30405362597cb10e44727df036aa1dd229550ad0
Yefry Moraga <jeffamed@gmail.com>
2021-12-30 18:16:01 -0600

    actualizaciones solicitadas parte 1

--- database/seeders/PermissionRolSeeder.php
+++ database/seeders/PermissionRolSeeder.php
@@ -183,156 +183,24 @@ class PermissionRolSeeder extends Seeder
             'rol_id' => '1',
         ]);
         DB::table('permission_rol')->insert([
-            'permission_id' => '1',
-            'rol_id' => '2',
+            'permission_id' => '43',
+            'rol_id' => '1',
         ]);
         DB::table('permission_rol')->insert([
-            'permission_id' => '2',
-            'rol_id' => '2',
+            'permission_id' => '44',
+            'rol_id' => '1',
         ]);
         DB::table('permission_rol')->insert([
-            'permission_id' => '3',
-            'rol_id' => '2',
+            'permission_id' => '45',
+            'rol_id' => '1',
         ]);
         DB::table('permission_rol')->insert([
-            'permission_id' => '4',
-            'rol_id' => '2',
+            'permission_id' => '46',
+            'rol_id' => '1',
         ]);
         DB::table('permission_rol')->insert([
-            'permission_id' => '5',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '6',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '7',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '8',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '9',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '10',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '11',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '12',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '13',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '14',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '15',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '16',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '17',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '18',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '19',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '20',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '21',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '22',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '23',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '24',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '25',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '26',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '27',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '28',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '29',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '30',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '31',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '32',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '33',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '34',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '35',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '36',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '37',
-            'rol_id' => '2',
-        ]);
-        DB::table('permission_rol')->insert([
-            'permission_id' => '38',
-            'rol_id' => '2',
+            'permission_id' => '47',
+            'rol_id' => '1',
         ]);
     }
 }
